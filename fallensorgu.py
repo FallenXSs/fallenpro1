5857d946e3dc6935ac5055edce23c8a5c5f414ab
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-09-17 11:31:51 +0400

    Update fallensorgu.py

--- fallensorgu.py
+++ fallensorgu.py
@@ -224,13 +224,13 @@ def handle_tcpro_command(message):
 ╟ OKUL ALANI: {okul_alani}
 ╟ İMAGE: {image}
 ╚═══════════════"""
-                bot.reply_to(message, reply_message)
-            else:
-                bot.reply_to(message, "TC kimlik numarası bulunamadı.")
-        except ValueError:
-            bot.reply_to(message, "API error 404 not found ERROR!.")
-    else:
-        bot.reply_to(message, "Bir hata oluştu. Lütfen daha sonra tekrar deneyin.")
+bot.reply_to(message, reply_message)
+else:
+bot.reply_to(message, "TC kimlik numarası bulunamadı.")
+except ValueError:
+bot.reply_to(message, "API error 404 not found ERROR!.")
+else:
+bot.reply_to(message, "Bir hata oluştu. Lütfen daha sonra tekrar deneyin.")
 
         
 @bot.message_handler(commands=['adres'])
